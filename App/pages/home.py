96a4d8b6799bf7d68d5f77c7ceec43511cda5518
Lis-tik <listik.sub@gmail.com>
2025-09-08 00:10:42 +0300

    development: ref input metadata

--- App/pages/home.py
+++ App/pages/home.py
@@ -16,72 +16,128 @@ class ContentButton(ft.ElevatedButton):
         self.style = ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=1))
 
 
+class HomePageClasster:
+    def __init__(self):
+        self.data = app_state._mediainfo.info_main_lib[app_state.global_path]
+        self.activeFile = None
+        self.newData = None
 
-def open_directory_dialog(mode):
-    root = Tk()
-    root.withdraw()  # Скрываем основное окно
-    root.attributes('-topmost', True)  # Поверх других окон
-    
-    # Открываем диалог выбора директории
-    directory = filedialog.askdirectory(title="Выберите папку")
-
-    if directory:  # Если папка выбрана
-        app_state.global_path = directory
-        app_state.files = [f for f in os.listdir(directory)]
+        self.info_video_ref = []
+        self.info_audio_ref = []
+        self.info_subtitle_ref = ft.Ref[ft.Text]()
+        self.data_ref = [self.info_audio_ref] #, self.info_audio_ref, self.info_subtitle_ref]
 
 
+    def data_currected(self, active):
+        return
+            
 
 
-def text_currected(data, name):   
-    Text = (f"{name}\n\nДетали:\nНомер в сборнике: №{data['index']}\nПолный путь: {data['name']}\n\nДанные видодорожек:\n")
+    def get_data_file(self, name):
+        self.activeFile = name
+        for value in self.data:
+            if self.activeFile in value['name']:
+                # Audio
+                for audio in value['audio']:
 
-    for video in data['video']:
-        Text += f'Разрешение сторон: {video['width']}x{video['height']}\nФормат цветопередачи: {video['pix_fmt']}\nПрофиль: {video['profile']}\n\n'
-    info_video_ref.current.value = Text
+                    audio_title_ref = ft.Ref[ft.TextField]()
 
-    Text = 'Данные аудиодорожек:\n'
-    for audio in data['audio']:
-        Text += f'Индекс: {audio['index']}\nОписание (имя): {audio['title']}\nЯзык: {audio['language']}\nКодек: {audio['codec_name']}\nКоличество каналов: {audio['channels']}\nБитрейт: {audio['bit_rate']}'
-        if int(audio['path']):
-            Text += f'\nПуть к директории: {audio['path']}'
-        Text += '\n\n'
-        
-    info_audio_ref.current.value = Text
-
-    Text = 'Данные субтитров:\n'
-    for subtitle in data['subtitle']:
-        Text += f'Описание (имя): {subtitle['title']}\nЯзык: {subtitle['language']}\nФормат: {subtitle['format']}'
-        if int(subtitle['path']):
-            Text += f'\nПуть к директории: {subtitle['path']}'
-        Text += '\n\n'
-
-    info_subtitle_ref.current.value = Text
+                    audio_field = ft.TextField(
+                        ref=audio_title_ref,  # Привязываем ref к полю
+                        hint_text="Введите имя аудиодорожки",
+                        value=audio['title'],  # Устанавливаем значение здесь
+                        width=300
+                    )                
+                    self.info_audio_ref.append(ft.Text("Имя (описание): ", size=15))
+                    self.info_audio_ref.append(audio_title_ref)
+                
 
 
 
-
-
-def get_data_file(name):
-    temp = app_state._mediainfo.info_main_lib[app_state.global_path]
-    for index, value in enumerate(temp):
-        if name in value['name']:
-            text_currected(value, name)
-            for dtref in data_ref:
-                dtref.current.update()
+            for refData in self.info_audio_ref:
+                refData.current.update()
             break
-
-info_video_ref = ft.Ref[ft.Text]()
-info_audio_ref = ft.Ref[ft.Text]()
-info_subtitle_ref = ft.Ref[ft.Text]()
-data_ref = [info_video_ref, info_audio_ref, info_subtitle_ref]
+                
 
 
-def Label():
-    return ft.Container(
-        content=ft.Text("ABR Maker", size=30, weight='bold'),
-        padding=5,
-    )
-     
+
+    def open_directory_dialog(self, mode):
+        root = Tk()
+        root.withdraw()  # Скрываем основное окно
+        root.attributes('-topmost', True)  # Поверх других окон
+        
+        # Открываем диалог выбора директории
+        directory = filedialog.askdirectory(title="Выберите папку")
+
+        if directory:  # Если папка выбрана
+            app_state.global_path = directory
+            app_state.files = [f for f in os.listdir(directory)]
+
+
+    def Information(self):
+        return ft.Row(
+            controls=[                    
+                # Правый блок - список файлов
+                ft.Column(
+                    controls = [
+                        ft.Container(
+                            content=ft.Column(
+                                controls=[ContentButton(f, on_click=lambda e, file=f: self.get_data_file(file)) for f in app_state.files],
+                                spacing=10,
+                                scroll=ft.ScrollMode.AUTO,
+                            ),
+                            expand=True,  # Занимает вторую половину ширины Row
+                            border=ft.border.all(1, ft.Colors.GREY_400),
+                            border_radius=10,
+                            padding=10,
+                            margin=ft.margin.symmetric(vertical=10),
+                        ),
+                    ]
+                ),
+                self.metaData()
+            ],
+            expand=True,  # Row растягивается на всю доступную ширину
+            spacing=10,   # Расстояние между блоками
+            vertical_alignment=ft.CrossAxisAlignment.START  # ← Ключевой параметр!
+        )
+    
+    def metaData(self):
+        return ft.Container(
+            content=ft.Column([
+                ft.Row(   # кнопки сверху
+                    [
+                        ft.ElevatedButton("Добавить аудиодорожку", on_click=lambda e: self.open_directory_dialog('audio')),
+                        ft.ElevatedButton("Добавить субтитры", on_click=lambda e: self.open_directory_dialog('subtitle'))
+                    ],
+                    alignment="start",
+                ),
+                ft.Text("Информация", size=20, weight="bold"),
+                ft.Divider(height=1),
+                self.distributionData()
+            ],
+            scroll=ft.ScrollMode.AUTO,
+            ),
+            padding=20,
+            bgcolor=ft.Colors.BLUE_GREY_50,
+            border_radius=15,
+            shadow=ft.BoxShadow(blur_radius=2),
+            expand=True,  # Занимает половину ширины Row
+        )
+    
+    def distributionData(self):
+        if not self.activeFile:
+            return ft.Container(
+                content=ft.Row([ft.Text("Выберете файл для просмотра информации", size=15)])
+            )
+
+        return ft.Container(
+            content=ft.Row(self.info_audio_ref)
+        )
+
+
+
+
+
 def navigation():
     return ft.Row(  # Лучше адаптируется для мобильных устройств
         controls=[
@@ -106,70 +162,39 @@ def navigation():
         run_spacing=10,  # Перенос на новую строку при нехватке места
     )
 
-def Information():
-    return ft.Row(
-        controls=[                    
-            # Правый блок - список файлов
-            ft.Column(
-                controls = [
-                    ft.Container(
-                        content=ft.Column(
-                            controls=[ContentButton(f, on_click=lambda e, file=f: get_data_file(file)) for f in app_state.files],
-                            spacing=10,
-                            scroll=ft.ScrollMode.AUTO,
-                        ),
-                        expand=True,  # Занимает вторую половину ширины Row
-                        border=ft.border.all(1, ft.Colors.GREY_400),
-                        border_radius=10,
-                        padding=10,
-                        margin=ft.margin.symmetric(vertical=10),
-                    ),
-                ]
-            ),
-            
-            # Левый блок - информационная панель
-            ft.Container(
-                content=ft.Column([
-                    ft.Row(   # кнопки сверху
-                        [
-                            ft.ElevatedButton("Изменить метаданные", on_click=lambda e: None),
-                            ft.ElevatedButton("Добавить аудиодорожку", on_click=lambda e: open_directory_dialog('audio')),
-                            ft.ElevatedButton("Добавить субтитры", on_click=lambda e: open_directory_dialog('subtitle'))
-                        ],
-                        alignment="start",
-                    ),
-                    ft.Text("Информация", size=20, weight="bold"),
-                    ft.Divider(height=1),
-                    ft.Text(ref=info_video_ref, size=16),
-                    ft.Divider(height=1),
-                    ft.Text(ref=info_audio_ref, size=16),
-                    ft.Divider(height=1),
-                    ft.Text(ref=info_subtitle_ref, size=16)
-                ],
-                scroll=ft.ScrollMode.AUTO,
-                ),
-                padding=20,
-                bgcolor=ft.Colors.BLUE_GREY_50,
-                border_radius=15,
-                shadow=ft.BoxShadow(blur_radius=2),
-                # margin=ft.margin.symmetric(vertical=10),
-                expand=True,  # Занимает половину ширины Row
-            ),
-        ],
-        expand=True,  # Row растягивается на всю доступную ширину
-        spacing=10,   # Расстояние между блоками
-        vertical_alignment=ft.CrossAxisAlignment.START  # ← Ключевой параметр!
+def Label():
+    return ft.Container(
+        content=ft.Text("ABR Maker", size=30, weight='bold'),
+        padding=5,
     )
 
-
-
 def get_home_page():
+    homePage = HomePageClasster()
     return ft.Column(
         controls=[
             Label(),
             navigation(),
-            Information(),
+            homePage.Information(),
         ],
         expand=True,
         spacing=20,
     )
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
